bbf6c8a5b5b31c58009e5184246eec7a1374dd06
Dimitri <dimitri.timoz@orange.fr>
2024-10-09 11:12:30 +0200

    VoyageurDuCommerce constructor

--- src/graphro/VoyageurDuCommerce.java
+++ src/graphro/VoyageurDuCommerce.java
@@ -1,5 +1,20 @@
 package graphro;
 
+import java.util.HashMap;
+
 public class VoyageurDuCommerce {
     
+    private GrapheMatrice graphe;
+    private int etiquettes[];
+
+    public VoyageurDuCommerce(GrapheMatrice graphe, int etiquettes[]) {
+        this.graphe = graphe;
+        if (this.etiquettes.length != graphe.taille()) {
+            throw new IllegalArgumentException("Le nombre d'étiquettes ne correspond pas au nombre de sommets du graphe");
+        }
+        this.etiquettes = etiquettes;
+    }
+
+    
+
 }
